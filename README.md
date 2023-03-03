1bdf15baa94d15e1a8ad039e2023c5b9a7a7cee8
Anuska86 <anasrm86@gmail.com>
2023-03-03 10:28:07 +0100

    Basics

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# proyecto_act_2
+# Proyecto 1
---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+let cors = require('cors')
+
+const app = express()
+ 
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/checkUser', (req, res) => {
+    let data = JSON.stringify(req.body);
+    console.log(data);
+    res.send('Data Received: ' + data);
+  })
+
+app.post('/createUser', (req, res) => {
+    let data = JSON.stringify(req.body);
+    console.log(data);
+    res.send('Data Received: ' + data);
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
