9c2d26a66c24a297290582e36c2e7dbc37b9b02c
Anuska86 <anasrm86@gmail.com>
2023-03-09 15:45:55 +0100

    Basics

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,54 @@
 //Index
 
-async function checkUser() {
-    await fetch('http://localhost:3000/checkUser', {
+(function() {
+  $('form > input').keyup(function() {
+
+      var empty = false;
+      $('form > input').each(function() {
+          if ($(this).val() == '') {
+              empty = true;
+          }
+      });
+
+      if (empty) {
+          $('#loginBtn').attr('disabled', 'disabled'); 
+      } else {
+          $('#loginBtn').removeAttr('disabled'); 
+      }
+  });
+})()
+
+function checkUser() {
+  let loginName = document.getElementById('user_input').value;
+  let loginPassword = document.getElementById('pass_input').value;
+  fetch(`http://localhost:8000/usuario/${loginName}/${loginPassword}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+  })
+    .then(response => response.json())
+    .then(response => response[0]?toogleChecks(true):toogleChecks(false))
+.catch(function(err) {
+    console.info(err );
+});
+}
+
+
+function toogleChecks(isValid){
+  if(isValid){
+    document.getElementById('admin_link').style.display="block";
+    document.getElementById('admin_link_alert').style.display="none";
+  }else{
+    document.getElementById('admin_link').style.display="none";
+    document.getElementById('admin_link_alert').style.display="block";
+  }
+}
+
+  function createUser(formData) {
+    fetch('http://localhost:8000/usuario', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -9,34 +56,30 @@ async function checkUser() {
         'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({
-        "loginName": document.getElementById('loginName').value,
-        "loginPassword": document.getElementById('loginPassword').value,
+        "nombre_usuario": formData.user_name,
+        "pass": formData.user_pass,
+        "email": formData.email,
+        "salario": formData.salary,
+        "tipo": "Usuario"
       })
     })
       .then(response => response.json())
-      .then(response => {
-      // HTTP 301 response
-      // HOW CAN I FOLLOW THE HTTP REDIRECT RESPONSE?
-      
-  })
-  .catch(function(err) {
-      console.info(err + " url: " + url);
-  });
-    //.then(response => console.log(JSON.stringify(response)))
   }
 
-  function createUser() {
-    fetch('http://localhost:3000/createUser', {
-      method: 'POST',
+  function updateUser(formData) {
+    fetch('http://localhost:8000/usuario', {
+      method: 'PUT',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({
-        "signUpName": document.getElementById('signUpName').value,
-        "signUpPassword": document.getElementById('signUpPassword').value,
-        "signUpPasswordRepeat": document.getElementById('signUpPasswordRepeat').value
+        "nombre_usuario": formData.user_name,
+        "pass": formData.user_pass,
+        "email": formData.email,
+        "salario": formData.salary,
+        "tipo": "Usuario"
       })
     })
       .then(response => response.json())
@@ -48,20 +91,25 @@ var selectedRow = null
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
-        if (selectedRow == null)
+        if (selectedRow == null){
             insertNewRecord(formData);
-        else
+            createUser(formData);
+        }
+        
+        else{
             updateRecord(formData);
+            updateUser(formData);
+        }
         resetForm();
     }
 }
 
 function readFormData() {
     var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
+    formData["user_name"] = document.getElementById("user_name").value;
+    formData["user_pass"] = document.getElementById("user_pass").value;
     formData["email"] = document.getElementById("email").value;
     formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
     return formData;
 }
 
@@ -69,38 +117,38 @@ function insertNewRecord(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
+    cell1.innerHTML = data.user_name;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
+    cell2.innerHTML = data.user_pass;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.email;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
+    cell4.innerHTML = data.salary;
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<button onClick="onEdit(this)" class="btn btn-info">Edit</button>
                        <button onClick="onDelete(this)" class="btn btn-danger">Delete</button>`;
 }
 
 function resetForm() {
-    document.getElementById("fullName").value = "";
+    document.getElementById("user_name").value = "";
+    document.getElementById("user_pass").value = "";
     document.getElementById("email").value = "";
     document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("user_name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("user_pass").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.email;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[0].innerHTML = formData.user_name;
+    selectedRow.cells[1].innerHTML = formData.user_pass;
+    selectedRow.cells[2].innerHTML = formData.email;
+    selectedRow.cells[3].innerHTML = formData.salary;
 }
 
 function onDelete(td) {
@@ -112,13 +160,13 @@ function onDelete(td) {
 }
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.getElementById("user_name").value == "") {
         isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
+        document.getElementById("user_nameValidationError").classList.remove("hide");
     } else {
         isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
+        if (!document.getElementById("user_nameValidationError").classList.contains("hide"))
+            document.getElementById("user_nameValidationError").classList.add("hide");
     }
     return isValid;
 }
